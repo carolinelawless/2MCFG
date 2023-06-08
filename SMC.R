2d0459ba5989a1469c1cc7309df481e2b79a2a8e
carolinelawless <101510489+carolinelawless@users.noreply.github.com>
2023-06-08 19:08:16 +0200

    Update SMC.R

--- SMC.R
+++ SMC.R
@@ -6,7 +6,7 @@ terminals<- c("a","b","c")
 
 M<- 500000
 sentences<- list()
-sentences[[1]]<- c("a","a","b","b","c","c","a","a","c","c")
+sentences[[1]]<- c("a","a","b","b","c","c","a","a","c","c","b","b")
 
 sentence<- sentences[[1]]
 sent= paste(sentence,collapse="")
