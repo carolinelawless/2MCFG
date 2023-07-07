05d47e98611590dd1cc191505154df728a89dcde
carolinelawless <101510489+carolinelawless@users.noreply.github.com>
2023-07-07 08:19:07 +0200

    Update grammar_analysis2.R

--- grammar_analysis2.R
+++ grammar_analysis2.R
@@ -8,8 +8,8 @@ g<- "copy"
 M<- 20000
 S<- 100
 alpha1<-alpha2<- 50
-b1<- 10 #Beta parameters for type = emission
-b2<- 1000
+b1<- 1000 #Beta parameters for type = emission
+b2<- 1
 c1<- 1 #Beta parameters for epsilon
 c2<- 1000
 name<- paste0(date,"_G=",g,"_M=",M,"_S=",S,"_b1=",b1)
