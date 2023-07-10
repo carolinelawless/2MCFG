dfd46f2bfd4b5ab34e8cc763cebb513fcb355875
carolinelawless <101510489+carolinelawless@users.noreply.github.com>
2023-07-10 02:08:31 +0200

    Update grammar_analysis2.R

--- grammar_analysis2.R
+++ grammar_analysis2.R
@@ -8,7 +8,7 @@ tic()
 
 g<- "copy"
 M<- 500000
-number_sentences<- 10
+number_sentences<- 50
 alpha1 <- 1 #scaling parameter for DP over nonterminals
 alpha2 <- 5 #scaling parameter for DP over rules
 b1<- 1 #Beta parameters for type = emission
