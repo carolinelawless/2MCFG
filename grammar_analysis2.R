63aa41c06e7a65b28a5d306e1f7a58b998c07321
carolinelawless <101510489+carolinelawless@users.noreply.github.com>
2023-07-13 15:37:31 +0200

    Update grammar_analysis2.R

--- grammar_analysis2.R
+++ grammar_analysis2.R
@@ -8,13 +8,13 @@ tic()
 
 g<- "doubles"
 M<- 50000
-number_sentences<- 25
+number_sentences<- 100
 len1<- 6
 len2<- 8
 len3<- 10
 len4<- 12
-alpha1 <- 10 #scaling parameter for DP over nonterminals
-alpha2 <- 10 #scaling parameter for DP over rules
+alpha1 <- 1 #scaling parameter for DP over nonterminals
+alpha2 <- 1 #scaling parameter for DP over rules
 b1<- 1 #Beta parameters for type = emission
 b2<- 1
 c1<- 1 #Beta parameters for epsilon
