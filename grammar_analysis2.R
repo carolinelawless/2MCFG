a83c4de282bf1d762f5c5d0a64bdaff2aecf3865
carolinelawless <101510489+carolinelawless@users.noreply.github.com>
2023-07-18 20:21:43 +0200

    Update grammar_analysis2.R

--- grammar_analysis2.R
+++ grammar_analysis2.R
@@ -6,15 +6,13 @@ library(LaplacesDemon)
 
 tic()
 
-g<- "doubles"
+g<- "copy"
 M<- 100000
 number_sentences<- 50
 len1<- 6
 len2<- 8
 len3<- 10
 len4<- 12
-len5<- 14
-len6<- 16
 
 alpha1 <- 1 #scaling parameter for DP over nonterminals
 alpha2 <- 1 #scaling parameter for DP over rules
@@ -23,16 +21,14 @@ b2<- 1
 c1<- 1 #Beta parameters for epsilon
 c2<- 1000
 
-description<- paste0("G=",g,"_M=",M,"_S=",number_sentences,"_alpha1=",alpha1,"_alpha2=",alpha2,"_b1=",b1,"_c2=",c2,"_len=",len1,"to",len6)
+description<- paste0("G=",g,"_M=",M,"_S=",number_sentences,"_alpha1=",alpha1,"_alpha2=",alpha2,"_b1=",b1,"_c2=",c2,"_len=",len1,"to",len4)
 print(description)
 
 terminals<- c("a","b","c")
 number_sentences1<- round(number_sentences/6)
 number_sentences2<- round(number_sentences/6)
 number_sentences3<- round(number_sentences/6)
-number_sentences4<- round(number_sentences/6)
-number_sentences5<- round(number_sentences/6)
-number_sentences6<- round(number_sentences/6)
+number_sentences4<- 1 - number_sentences1 - number_sentences2 - number_sentences3
 
 sentences<- list()
 
@@ -63,22 +59,6 @@ for(i in 1:number_sentences3){
 }
 for(i in 1:number_sentences4){
   sent_short<- sample(terminals,len4/2,replace = TRUE)
-  if(g=="copy"){
-    sentences[[length(sentences)+1]]<- rep(sent_short,2)
-  }else if(g=="doubles"){
-    sentences[[length(sentences)+1]]<- rep(sent_short,each=2)
-  }
-}
-for(i in 1:number_sentences5){
-  sent_short<- sample(terminals,len5/2,replace = TRUE)
-  if(g=="copy"){
-    sentences[[length(sentences)+1]]<- rep(sent_short,2)
-  }else if(g=="doubles"){
-    sentences[[length(sentences)+1]]<- rep(sent_short,each=2)
-  }
-}
-for(i in 1:number_sentences6){
-  sent_short<- sample(terminals,len6/2,replace = TRUE)
   if(g=="copy"){
     sentences[[length(sentences)+1]]<- rep(sent_short,2)
   }else if(g=="doubles"){
