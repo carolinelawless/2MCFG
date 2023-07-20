461c154294266ecb63e3e27798df663dfd2e8211
carolinelawless <101510489+carolinelawless@users.noreply.github.com>
2023-07-20 19:30:39 +0200

    Update grammar_analysis2.R

--- grammar_analysis2.R
+++ grammar_analysis2.R
@@ -6,27 +6,35 @@ library(LaplacesDemon)
 
 tic()
 
-g<- "copy"
-M<- 1000
+g<- "doubles"
+M<- 50000
 number_sentences<- 25
-len<- 10
-
-alpha1 <- 0.001 #scaling parameter for DP over nonterminals
-alpha2 <- 0.001 #scaling parameter for DP over rules
+len1<- 6
+len2<- 8
+len3<- 10
+len4<- 12
+alpha1 <- 10 #scaling parameter for DP over nonterminals
+alpha2 <- 10 #scaling parameter for DP over rules
 b1<- 1 #Beta parameters for type = emission
 b2<- 1
 c1<- 1 #Beta parameters for epsilon
-c2<- 100000
+c2<- 1000
 
-description<- paste0("G=",g,"_M=",M,"_S=",number_sentences,"_alpha1=",alpha1,"_alpha2=",alpha2,"_b1=",b1,"_c2=",c2,"_len=",len)
+description<- paste0("G=",g,"_M=",M,"_S=",number_sentences,"_alpha1=",alpha1,"_alpha2=",alpha2,"_b1=",b1,"_c2=",c2,"_len=",len1,"to",len4)
 print(description)
 
 terminals<- c("a","b","c")
+number_sentences1<- round(number_sentences/6)
+number_sentences2<- round(number_sentences/6)
+number_sentences3<- round(number_sentences/6)
+number_sentences4<- number_sentences - number_sentences1 - number_sentences2 - number_sentences3
+
+
 
 sentences<- list()
 
-for(i in 1:number_sentences){
-  sent_short<- sample(terminals,len/2,replace = TRUE)
+for(i in 1:number_sentences1){
+  sent_short<- sample(terminals,len1/2,replace = TRUE)
   if(g=="copy"){
     sentences[[length(sentences)+1]]<- rep(sent_short,2)
   }else if(g=="doubles"){
@@ -34,6 +42,32 @@ for(i in 1:number_sentences){
   }
 }
 
+for(i in 1:number_sentences2){
+  sent_short<- sample(terminals,len2/2,replace = TRUE)
+  if(g=="copy"){
+    sentences[[length(sentences)+1]]<- rep(sent_short,2)
+  }else if(g=="doubles"){
+    sentences[[length(sentences)+1]]<- rep(sent_short,each=2)
+  }
+}
+
+for(i in 1:number_sentences3){
+  sent_short<- sample(terminals,len3/2,replace = TRUE)
+  if(g=="copy"){
+    sentences[[length(sentences)+1]]<- rep(sent_short,2)
+  }else if(g=="doubles"){
+    sentences[[length(sentences)+1]]<- rep(sent_short,each=2)
+  }
+}
+
+for(i in 1:number_sentences4){
+  sent_short<- sample(terminals,len4/2,replace = TRUE)
+  if(g=="copy"){
+    sentences[[length(sentences)+1]]<- rep(sent_short,2)
+  }else if(g=="doubles"){
+    sentences[[length(sentences)+1]]<- rep(sent_short,each=2)
+  }
+}
 
 source("SMC.R")
 source("functions2.R")
