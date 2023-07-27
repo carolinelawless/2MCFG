7af9e48186855834f15a6d6336b2197369c3287b
carolinelawless <101510489+carolinelawless@users.noreply.github.com>
2023-07-27 15:22:50 +0200

    Update grammar_analysis2.R

--- grammar_analysis2.R
+++ grammar_analysis2.R
@@ -7,9 +7,9 @@ library(LaplacesDemon)
 tic()
 
 g<- "copy"
-M<- 10000
-number_sentences<- 10
-len<- 30
+M<- 1000
+number_sentences<- 100
+len<- 10
 alpha1 <- 0.1 #scaling parameter for DP over nonterminals
 alpha2 <- 0.1 #scaling parameter for DP over rules
 b1<- 10 #Beta parameters for type = emission
