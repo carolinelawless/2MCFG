c13247d57f90ec2a0e355b40ebe833c6690c0ec6
carolinelawless <101510489+carolinelawless@users.noreply.github.com>
2023-08-06 15:08:05 +0200

    Update grammar_analysis2.R

--- grammar_analysis2.R
+++ grammar_analysis2.R
@@ -11,7 +11,7 @@ M<- 1000
 number_sentences<- 200
 len<- 30
 alpha1 <- 0.5 #scaling parameter for DP over nonterminals
-alpha2 <- 1 #scaling parameter for DP over rules
+alpha2 <- 1.5 #scaling parameter for DP over rules
 b1<- 10 #Beta parameters for type = emission
 b2<- 10
 c1<- 1 #Beta parameters for epsilon
