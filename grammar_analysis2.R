af41541590d3a2ccdea21fe04b002bb5b8893170
carolinelawless <101510489+carolinelawless@users.noreply.github.com>
2023-08-11 10:24:42 +0200

    Update grammar_analysis2.R

--- grammar_analysis2.R
+++ grammar_analysis2.R
@@ -7,12 +7,12 @@ library(LaplacesDemon)
 start<- Sys.time()
 tic()
 
-g<- "copy"
-M<- 500
+g<- "monkey"
+M<- 1
 number_sentences<- 200
 len<- 30
-alpha1 <- 0.5 #scaling parameter for DP over nonterminals
-alpha2 <- 5 #scaling parameter for DP over rules
+alpha1 <- 0.1 #scaling parameter for DP over nonterminals
+alpha2 <- 0.1 #scaling parameter for DP over rules
 b1<- 10 #Beta parameters for type = emission
 b2<- 10
 c1<- 1 #Beta parameters for epsilon
