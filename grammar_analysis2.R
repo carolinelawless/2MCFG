c1353a40230f73325c74e6e5abe106a8e40b0f80
carolinelawless <101510489+carolinelawless@users.noreply.github.com>
2023-09-04 16:21:03 +0200

    Update grammar_analysis2.R

--- grammar_analysis2.R
+++ grammar_analysis2.R
@@ -9,7 +9,7 @@ tic()
 
 g<- "copy"
 M<- 1000
-number_sentences<- 175
+number_sentences<- 200
 len<- 30
 alpha1 <- 0.5 #scaling parameter for DP over nonterminals
 alpha2 <- 0.5 #scaling parameter for DP over rules
