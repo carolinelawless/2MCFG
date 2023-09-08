db7e25fe50e5e1e43a5cd305badcd81a49889812
carolinelawless <101510489+carolinelawless@users.noreply.github.com>
2023-09-08 11:00:49 +0200

    Update grammar_analysis2.R

--- grammar_analysis2.R
+++ grammar_analysis2.R
@@ -9,7 +9,7 @@ tic()
 
 g<- "mix"
 M<- 1000
-number_sentences<- 200
+number_sentences<- 100
 len<- 30
 alpha1 <- 0.5 #scaling parameter for DP over nonterminals
 alpha2 <- 0.5 #scaling parameter for DP over rules
